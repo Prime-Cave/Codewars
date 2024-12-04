d833277fcb0c58b0747b9fdad8ac59f41036b901
Prime-Cave <tomisin.akinfemiwa@gmail.com>
2024-12-04 14:16:03 +0200

    sadly i need chatgpt help

--- js /6kyu/ipValidation.js
+++ js /6kyu/ipValidation.js
@@ -22,3 +22,33 @@
 // 4. check if teh vakues are greater than 0 or less or equal to 255
 // 5. If conditions are met then append array into string and return it
 
+function isValidIP(str) {
+   const strArray = str.split('.')
+   if(strArray.length !== 4){
+    return false 
+   }
+    for (let x =0; x < strArray.length; x++){
+        let octet = strArray[x]
+            
+
+            if(!/^\d+$/.test(octet) ||(octet.length > 1 && octet.startsWith('0'))){
+                return false 
+            }
+
+            let stringToNumber = Number(strArray[x])
+            if ( stringToNumber < 0 || stringToNumber > 255){
+                return false
+            }
+
+    }
+    return true;
+  }
+  
+console.log(/^\d+$/.test("122"))
+
+// regex used 
+
+//  1. ^ : makes sure it matches the start of the string
+//  2. \d : makes sure it matches number 0-9
+//  3. + : makes sure it has at least one value 
+//  4. $ : makes sure it matches the end of the string
