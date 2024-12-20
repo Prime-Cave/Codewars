c4213030ef30b20ccf4eca22707a4533b35b6820
Prime-Cave <tomisin.akinfemiwa@gmail.com>
2024-12-20 13:45:57 +0200

    New Codewars added

--- js /5kyu /humanReadableNumbers.js
+++ js /5kyu /humanReadableNumbers.js
@@ -6,3 +6,27 @@
 // The maximum time never exceeds 359999 (99:59:59)
 
 // You can find some examples in the test fixtures.
+
+function humanReadable (seconds) {
+    const hours = Math.floor(seconds / 3600); // Calculate hours
+    const minutes = Math.floor((seconds % 3600) / 60); // Calculate remaining minutes
+    const remainingSeconds = seconds % 60; // Calculate remaining seconds
+
+    // change to two digits return value 
+    const paddedHours = String(hours).padStart(2, '0');
+    const paddedMinutes = String(minutes).padStart(2, '0');
+    const paddedSeconds = String(remainingSeconds).padStart(2, '0');
+
+    return `${paddedHours}:${paddedMinutes}:${paddedSeconds}`;
+}
+
+//REFACTOR 
+function humanReadable (seconds) {
+    const paddedHours = String(Math.floor(seconds / 3600)).padStart(2, '0');
+    const paddedMinutes = String(Math.floor((seconds % 3600) / 60)).padStart(2, '0');
+    const paddedSeconds = String(seconds % 60).padStart(2, '0');
+
+    return `${paddedHours}:${paddedMinutes}:${paddedSeconds}`;
+}
+
+console.log(humanReadable(0));
