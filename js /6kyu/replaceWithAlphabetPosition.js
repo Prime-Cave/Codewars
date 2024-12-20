d66e319eab0f761a3c72a687d7a1c19e54c5362a
Prime-Cave <tomisin.akinfemiwa@gmail.com>
2024-12-20 14:11:15 +0200

    New 6 kyu

---
+++ js /6kyu/replaceWithAlphabetPosition.js
@@ -0,0 +1,10 @@
+// Welcome.
+// In this kata you are required to, given a string, replace every letter with its position in the alphabet.
+// If anything in the text isn't a letter, ignore it and don't return it.
+// "a" = 1, "b" = 2, etc.
+//link = https://www.codewars.com/kata/546f922b54af40e1e90001da/train/javascript
+
+function alphabetPosition(text) {
+    
+    return text;
+}
