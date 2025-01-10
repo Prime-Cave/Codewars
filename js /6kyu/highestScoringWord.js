6a2fa3f2c29ef6683f5f5ddde3a73fef73778ee4
Prime-Cave <tomisin.akinfemiwa@gmail.com>
2025-01-10 14:52:35 +0200

    Some wars

--- js /6kyu/highestScoringWord.js
+++ js /6kyu/highestScoringWord.js
@@ -5,3 +5,42 @@
 // If two words score the same, return the word that appears earliest in the original string.
 // All letters will be lowercase and all inputs will be valid.
 
+// psuedocode
+// 1. get the sentence
+// 2. split the sentence into an array of words
+// 3. store the array of words
+// 4. loop over the array of words
+// 5. then loop over each word
+// 6. for each word get the unicode number using "a".charCodeAt(0)
+
+function high(x) {
+  const arrayOfWords = [...x.split(" ")];
+  let scores = [];
+
+  for (word of arrayOfWords) {
+    let wordScore = 0;
+    for (character of word) {
+      wordScore += character.charCodeAt(0) - 96;
+    }
+    scores.push(wordScore);
+  }
+  const max = Math.max(...scores);
+
+  return arrayOfWords[scores.indexOf(max)]
+
+
+//   console.log(scores);
+//   let arrOfIndicies = [];
+//   for (x = 0; x < scores.length; x++) {
+//     if(scores[x] >= max){
+//         arrOfIndicies.push(scores.indexOf(scores[x]))
+//         scores.splice(scores.indexOf(scores[x]), 1,0)
+//     }
+//   }
+//   let highScoreWord = []
+//   for(x =0; x < arrOfIndicies.length; x++){
+//     highScoreWord.push(arrayOfWords[arrOfIndicies[x]])
+//   }
+// return highScoreWord.join(" ")
+}
+console.log(high("man i need a taxi up taxi to ubud"));
