96028754b2d3b1d6f481b954b1a6a107a4f5644a
Prime-Cave <tomisin.akinfemiwa@gmail.com>
2025-01-29 11:05:46 +0200

    Did it again

--- js /6kyu/WriteNumberInExpandedForm.js
+++ js /6kyu/WriteNumberInExpandedForm.js
@@ -1,5 +1,15 @@
 // You will be given a number and you will need to return it as a string in Expanded Form. 
 
 function expandedForm(num) {
-    // Your code here
-  }
+    let newValue = [...num.toString()].map((digit, index, arr) =>{
+        if(digit > 0){
+            let tempArr = arr.slice(index).fill(0,1).join("")
+            return tempArr
+        }
+    })
+    let endResult = newValue.filter(str => str !== undefined).join(' + ')
+    return endResult
+}
+
+//console.log(expandedForm(12)); // should return '10 + 2'
+console.log(expandedForm(70304)); 
