01cb70562cd412488333f1eb3a3baf1305945cda
Prime-Cave <tomisin.akinfemiwa@gmail.com>
2025-02-05 10:54:35 +0200

    Es6 Classes

--- js /8kyu/ES6Classes#1.js
+++ js /8kyu/ES6Classes#1.js
@@ -11,5 +11,18 @@
 // Have fun! :D
 
 class Person {
-    // Get coding in ES6 :D
+    constructor(firstName = 'John', lastName = 'Doe', age = 0, gender = 'Male'){
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.age = age;
+        this.gender = gender;
+    }
+
+    sayFullName(){
+        return `${this.firstName} ${this.lastName}`;
+    }
+
+    static greetExtraTerrestrials(raceName){
+        return `Welcome to Planet Earth ${raceName}`;
+    }
   }
